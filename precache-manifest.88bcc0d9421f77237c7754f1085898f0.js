c7840b2bd68dfc1612889c6d469f1e8884d9039c
thaitwo <tmtu90@gmail.com>
2019-12-01 18:55:38 -0800

    Updates

---
+++ precache-manifest.88bcc0d9421f77237c7754f1085898f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb409ea006187142b3407e3705c2d25",
+    "url": "/lifequality/index.html"
+  },
+  {
+    "revision": "1a506228b912d2c852c2",
+    "url": "/lifequality/static/css/main.c105af4a.chunk.css"
+  },
+  {
+    "revision": "1a903ec6c87e3731e66a",
+    "url": "/lifequality/static/js/2.b10c6ab2.chunk.js"
+  },
+  {
+    "revision": "1a506228b912d2c852c2",
+    "url": "/lifequality/static/js/main.65d1e38c.chunk.js"
+  },
+  {
+    "revision": "927666c805370b73dc12",
+    "url": "/lifequality/static/js/runtime-main.9f4c9c3f.js"
+  }
+]);
